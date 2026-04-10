2c24174f410b0a480316972cc97c1d7bafef1f8d
Lucas Mundim <lmundim@plainsight.ai>
2026-04-09 22:02:37 -0300

    Remove unused FilterFaceblurConfig import from tests
    Signed-off-by: Lucas Mundim <lmundim@plainsight.ai>

--- tests/test_process_frame.py
+++ tests/test_process_frame.py
@@ -11,7 +11,7 @@ import pytest
 import numpy as np
 from unittest.mock import MagicMock, patch
 from openfilter.filter_runtime.filter import Frame
-from filter_faceblur.filter import FilterFaceblur, FilterFaceblurConfig
+from filter_faceblur.filter import FilterFaceblur
 from filter_faceblur.model.model import FaceBlur
 
 
